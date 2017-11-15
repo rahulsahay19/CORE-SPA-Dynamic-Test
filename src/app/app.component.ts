0812a0b7778db2427e182b77b487b3c66f48e039
Rahul Sahay <rahulsahay19@hotmail.com>
2017-11-15 14:32:24 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    //And, this iterates over to see the location dynamically and load the same.
+    this.files.forEach(element => {
+      return import (element).then((comp:any)=>{
+        return comp.default;
+      }); 
+    });
+ 
+  }
   title = 'app';
+
+  //For instance, this is the files collection
+  files =['./module1/main-bundle']
 }
